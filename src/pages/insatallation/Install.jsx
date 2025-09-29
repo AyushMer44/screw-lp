5860217d0938444a99883c04f491a103e94a02ce
Yash Bhatt <30y.bhatt@gmail.com>
2025-09-29 23:33:40 +0530

    Installation completed

--- src/pages/insatallation/Install.jsx
+++ src/pages/insatallation/Install.jsx
@@ -1,7 +1,47 @@
 import React from "react";
+import backgroundImage from "../../assets/about/about-wallpaper.png";
+import styles from "./Install.module.scss";
+import Footer from "../../components/footer/Footer";
 
 const Install = () => {
-  return <div>Install</div>;
+  return (
+    <div>
+      <div className={styles.install}>
+        <div
+          className={styles.heroSection}
+          style={{ backgroundImage: `url(${backgroundImage})` }}
+        >
+          <div className={styles.overlay} />
+          <h1 className={styles.title}>Installation</h1>
+        </div>
+      </div>
+
+      <div className={styles.contentSection}>
+        <h2 className={styles.subtitle}>Glimpse Of Installation</h2>
+
+        <div className={styles.imageGrid}>
+          <div className={styles.div1}>1</div>
+          <div className={styles.div2}>2</div>
+          <div className={styles.div3}>3</div>
+          <div className={styles.div4}>4</div>
+          <div className={styles.div5}>5</div>
+        </div>
+      </div>
+
+      <div className={styles.contentSection}>
+        <h2 className={styles.subtitle}>PROCESS</h2>
+
+        <div className={styles.imageGrid}>
+          <div className={styles.div1}>1</div>
+          <div className={styles.div2}>2</div>
+          <div className={styles.div3}>3</div>
+          <div className={styles.div4}>4</div>
+          <div className={styles.div5}>5</div>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
 };
 
 export default Install;
