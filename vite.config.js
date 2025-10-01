66cd5a8adc42d07425a1ec34a08af51d622ea403
Yash Bhatt <30y.bhatt@gmail.com>
2025-10-01 13:59:24 +0530

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+// Vite config
 export default defineConfig({
+  base: "/", // Set base path if hosted in a subdirectory
   plugins: [react()],
-})
+  build: {
+    outDir: "dist", // Output directory for build files
+  },
+  publicDir: "public", // Static assets folder
+});
